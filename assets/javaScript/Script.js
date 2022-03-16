cb78aec8dde9dbd8920509db64b7dfb5c072670d
latiifaa <100519717+latiifaa@users.noreply.github.com>
2022-03-16 19:38:52 +0300

    add calculations

--- assets/javaScript/Script.js
+++ assets/javaScript/Script.js
@@ -2,3 +2,9 @@ alert('لطيفه')
 console.log(17)
 console.log('%c بعد شهرين اكمل 18', 'color: red');
 // Javascript is not Java
+
+let budget = 20;
+console.log(budget >= 30 && budget <= 80)
+
+budget += 30
+console.log(budget >= 30 && budget <= 80)
