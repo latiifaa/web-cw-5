6bf49a37b89cd07c9a5e327ba53d8099edfef2e5
latiifaa <100519717+latiifaa@users.noreply.github.com>
2022-03-18 00:07:03 +0300

    if-else & switch

--- assets/javaScript/Script.js
+++ assets/javaScript/Script.js
@@ -8,3 +8,41 @@ console.log(budget >= 30 && budget <= 80)
 
 budget += 30
 console.log(budget >= 30 && budget <= 80)
+
+
+
+let grade = prompt(" enter your grade")
+if (grade >=90) {
+console.log ("لقد حصلت على امتياز 🥳");
+}else if (grade >=80) {
+console.log ("لقد حصلت على جيد جداً🤩");
+}else if (grade >=70) {
+console.log ("لقد حصلت على جيد🙂");
+}else if (grade >=60) {
+console.log ("لقد حصلت على مقبول😕");
+}else if (grade >=50) {
+console.log ("لقد حصلت على ضعيف☹️");
+}else {
+    console.log ("راسب💔");
+}
+
+
+switch(true) {
+    case grade >=90:
+    console.log ("لقد حصلت على امتياز 🥳");
+    break;
+    case grade >=80:
+    console.log ("لقد حصلت على جيد جداً🤩");
+    break;
+    case grade >=70:
+    console.log ("لقد حصلت على جيد🙂");
+    break;
+    case grade >=60:
+    console.log ("لقد حصلت على مقبول😕");
+    break;
+    case grade >=50:
+    console.log ("لقد حصلت على ضعيف☹️");
+    break;
+    default:
+    console.log ("راسب💔");
+}
